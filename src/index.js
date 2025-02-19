058d83452e0c86fa414ee1bdc5bad3dcf8b34afc
Lloyd <you@example.com>
2025-02-19 15:29:39 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constant"
+
+
+// (async () => {
+//   try {
+//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//   } catch (error) {
+//     console.error("ERROR: ", error)
+//     throw err
+//   }
+// })()
