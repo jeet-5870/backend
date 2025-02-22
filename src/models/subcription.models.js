07819389d210aa629c39c4cdc935dfa7e25b00ed
Lloyd <you@example.com>
2025-02-23 00:47:54 +0530

    add subscription

---
+++ src/models/subcription.models.js
@@ -0,0 +1,16 @@
+import mongoose { schema } from "mongoose"
+
+const subscriptionSchema = new mongoose.Schema({
+  subscriber: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  channel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+}, { timestamps: true })
+
+export const subscription = mongoose.model("subscription", subscriptionSchema)
